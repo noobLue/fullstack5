7e4e1d6593d63ad879add2abbe2f07f61cf8d50a
nooblue <null>
2024-03-28 10:21:21 +0200

    added testhelper

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -6,62 +6,12 @@ const app = require('../app')
 
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const blogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-    },
-    {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-    },
-    {
-        _id: "5a422b891b54a676234d17fa",
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-        likes: 10,
-        __v: 0
-    },
-    {
-        _id: "5a422ba71b54a676234d17fb",
-        title: "TDD harms architecture",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-        likes: 0,
-        __v: 0
-    },
-    {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-        __v: 0
-    }  
-]
+const testHelper = require('./test_helper')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
 
-    for(const b of blogs)
+    for(const b of testHelper.initBlogs)
         await (new Blog(b)).save()
 })
 
@@ -75,7 +25,7 @@ test('blogs are returned as json', async () => {
 test('correct amount of blogs', async () => {
     const res = await api.get('/api/blogs')
 
-    assert.strictEqual(res.body.length, blogs.length)
+    assert.strictEqual(res.body.length, testHelper.initBlogs.length)
 })
 
 test('has id property', async () => {
@@ -98,11 +48,11 @@ test('a blog is correctly added', async () => {
         .expect(201)
         .expect('Content-Type', /application\/json/)
 
-    const res = await api.get('/api/blogs')
+    const blogs = await testHelper.getBlogs()
 
-    assert.strictEqual(res.body.length, blogs.length + 1)
+    assert.strictEqual(blogs.length, testHelper.initBlogs.length + 1)
 
-    assert.strictEqual(res.body[blogs.length].title, blog.title)
+    assert.strictEqual(blogs[testHelper.initBlogs.length].title, blog.title)
 })
 
 test('likes are defaulted to 0', async () => {
@@ -117,9 +67,9 @@ test('likes are defaulted to 0', async () => {
         .expect(201)
         .expect('Content-Type', /application\/json/)
 
-    const res = await api.get('/api/blogs')
+        const blogs = await testHelper.getBlogs()
 
-    assert.strictEqual(res.body[blogs.length].likes, 0)
+    assert.strictEqual(blogs[testHelper.initBlogs.length].likes, 0)
 })
 
 test('missing title is not added', async () => {
